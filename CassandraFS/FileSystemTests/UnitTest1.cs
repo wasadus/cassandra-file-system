6a50749de260f842e22ea64ded4c60fcabfb958d
wasadus <wasadus@gmail.com>
2021-04-12 18:25:09 +0500

    Added tests projects

---
+++ CassandraFS/FileSystemTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+
+using Newtonsoft.Json;
+
+using NUnit.Framework;
+
+namespace FileSystemTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
